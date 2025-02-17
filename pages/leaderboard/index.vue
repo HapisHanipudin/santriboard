8bfbbac1a68852a8a3123bbfbf690cc612fe4e8c
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2025-02-17 09:18:12 +0700

    feat(tabs)

--- pages/leaderboard/index.vue
+++ pages/leaderboard/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <TabWrapper />
+    <TabWrapper>
+      <Tab title="tahfizh">tahfizh</Tab>
+      <Tab title="it">it</Tab>
+      <Tab title="karakter">karakter</Tab>
+      <Tab title="bahasa">bahasa</Tab>
+    </TabWrapper>
   </div>
 </template>
 
