6a87214e9d10d8cf8da2382d360547c303bb9390
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2025-02-17 15:03:05 +0700

    del: deleted all server side functions

--- server/db/index.ts
+++
@@ -1,4 +0,0 @@
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient().$extends(withAccelerate());
