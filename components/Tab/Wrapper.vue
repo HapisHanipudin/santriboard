b357889709ad2cbe0f5a7aefe81fd2c3d8926bd0
Azzam Hudiya <hudiya777@gmail.com>
2025-02-17 09:18:05 +0700

    pppp

--- components/Tab/Wrapper.vue
+++ components/Tab/Wrapper.vue
@@ -2,10 +2,18 @@
   <div>
     <div class="p-2 rounded-3xl bg-zinc-700">
       <div class="w-full grid grid-cols-4 relative">
-        <span @click="selectTitle(title)" v-for="(title, index) in props.titles" :key="index" class="text-center z-10 py-2">
+        <span 
+          @click="selectTitle(title)" 
+          v-for="(title, index) in props.titles" 
+          :key="index" 
+          class="text-center z-10 py-2 cursor-pointer" 
+        >
           <span class="text-2xl capitalize">{{ title }}</span>
         </span>
-        <div :style="{ transform: `translateX(${props.titles.indexOf(selectedTitle)}00%)` }" class="w-1/4 h-full bg-[#3E62FE] absolute top-0 left-0 -z-0 rounded-2xl transition-transform duration-500 ease-in-out"></div>
+        <div 
+          :style="{ transform: `translateX(${props.titles.indexOf(selectedTitle)}00%)` }" 
+          class="w-1/4 h-full bg-[#3E62FE] absolute top-0 left-0 -z-0 rounded-2xl transition-transform duration-500 ease-in-out"
+        ></div>
       </div>
     </div>
     <div>
@@ -15,6 +23,8 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
+
 const props = defineProps({
   titles: {
     type: Array,
--- pages/leaderboard/index.vue
+++ pages/leaderboard/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <TabWrapper />
+    ini leaderboard
   </div>
 </template>
 
