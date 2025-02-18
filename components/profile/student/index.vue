7ce807aebaaef41f9e33c7fef51aece5e89f81da
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2025-02-18 10:55:43 +0700

    feat: profile page mockup

---
+++ components/profile/student/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="grid grid-cols-1 lg:grid-cols-5 grid-rows-9 gap-4">
+    <ProfileStudentCard class="col-span-3 row-span-2" :student="student" />
+    <ProfileStudentAchievement class="col-span-3 row-span-4" :student="student" />
+    <ProfileStudentDetail class="col-span-2 row-span-6 grid-rows-subgrid lg:col-start-4 lg:row-start-1" :student="student" />
+    <ProfileStudentProject class="col-span-3 row-span-3" :student="student" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Student {
+  id: number;
+  name: string;
+  nickname: string;
+  image: string;
+  birthdate: string;
+}
+
+const { student } = defineProps({
+  student: {
+    type: Object as PropType<Student>,
+    default: {
+      id: 1,
+      name: "Muhammad Iqbal Mubarok",
+      nickname: "Iqbal",
+      image: "https://avatars.githubusercontent.com/u/57764837?v=4",
+      birthdate: "2003-03-30",
+    },
+  },
+});
+</script>
+
+<style></style>
--- pages/leaderboard/index.vue
+++ pages/leaderboard/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <TabWrapper>
-      <Tab title="tahfizh"><Leaderboard /></Tab>
+    <TabWrapper :tabs="tabTitles">
+      <Tab title="allRounded"><Leaderboard /></Tab>
+      <Tab title="tahfizh"><Leaderboard title="tahfizh" /></Tab>
       <Tab title="it"><Leaderboard title="it" /></Tab>
       <Tab title="karakter"><Leaderboard title="karakter" /></Tab>
       <Tab title="bahasa"><Leaderboard title="bahasa" /></Tab>
@@ -9,6 +10,34 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const tabTitles = ref([
+  {
+    title: "allRounded",
+    icon: "fa:users",
+    display: "All Rounded",
+  },
+  {
+    title: "tahfizh",
+    icon: "el:book",
+    display: "Tahfizh",
+  },
+  {
+    title: "it",
+    icon: "mynaui:desktop-solid",
+    display: "IT",
+  },
+  {
+    title: "karakter",
+    icon: "streamline:brain-cognitive-solid",
+    display: "Karakter",
+  },
+  {
+    title: "bahasa",
+    icon: "fa6-solid:language",
+    display: "Bahasa",
+  },
+]);
+</script>
 
 <style></style>
