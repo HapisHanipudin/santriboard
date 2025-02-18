6f73114e2a20eb05ba0ccb02f64618aaaf96084b
Azzam Hudiya <hudiya777@gmail.com>
2025-02-18 10:58:46 +0700

    okelah mayan

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -2,71 +2,72 @@
   <div>
     <div class="container mx-auto p-4">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <!-- Profile Section -->
-        <div class="col-span-1 bg-gray-800 p-4 rounded-lg">
-          <div class="flex items-center">
-            <img
-              alt="Profile picture of Preston Delacruz"
-              class="rounded-full w-24 h-24"
-              height="100"
-              src="https://app.dreamapps.id/storage/students/photo/232403208.webp"
-              width="100"
-            />
-            <div class="ml-4">
-              <p class="text-sm text-gray-400">Nama</p>
-              <h2 class="text-lg">Hudskuy</h2>
-              <p class="text-sm text-gray-400">Title</p>
-              <h3 class="text-lg">Si Paling Otot</h3>
-              <p class="text-sm text-gray-400">Umur</p>
-              <h3 class="text-lg">1000 Tahun</h3>
-            </div>
-          </div>
-        </div>
+<!-- Profile Section -->
+<div class="col-span-1 bg-gray-800 p-4 rounded-3xl flex justify-center items-center">
+  <div class="flex items-center">
+    <img
+      alt="Profile picture of Preston Delacruz"
+      class="rounded-full w-24 h-24"
+      height="100"
+      src="https://app.dreamapps.id/storage/students/photo/232403208.webp"
+      width="100"
+    />
+    <div class="ml-4 text-left">
+      <p class="text-sm text-gray-400">Nama</p>
+      <h2 class="text-lg">Hudskuy</h2>
+      <p class="text-sm text-gray-400">Title</p>
+      <h3 class="text-lg">Si Paling Otot</h3>
+      <p class="text-sm text-gray-400">Umur</p>
+      <h3 class="text-lg">1000 Tahun</h3>
+    </div>
+  </div>
+</div>
+
         <!-- Stats Section -->
         <div
           class="col-span-1 md:col-span-2 grid grid-cols-2 md:grid-cols-4 gap-4"
         >
-          <div class="bg-gray-800 p-4 rounded-lg text-center">
+          <div class="bg-gray-800 p-4 rounded-3xl text-center">
             <div
-              class="bg-blue-500 w-12 h-12 rounded-full mx-auto flex items-center justify-center"
+              class="bg-blue-500 w-12 h-12 rounded-[20px] mx-auto flex items-center justify-center"
             >
               <i class="fas fa-user"> </i>
             </div>
             <h3 class="text-xl font-bold mt-2">3</h3>
             <p class="text-sm">Pondok</p>
           </div>
-          <div class="bg-gray-800 p-4 rounded-lg text-center">
+          <div class="bg-gray-800 p-4 rounded-3xl text-center">
             <div
-              class="bg-blue-500 w-12 h-12 rounded-full mx-auto flex items-center justify-center"
+              class="bg-blue-500 w-12 h-12 rounded-[20px] mx-auto flex items-center justify-center"
             >
               <i class="fas fa-user"> </i>
             </div>
             <h3 class="text-xl font-bold mt-2">1</h3>
             <p class="text-sm">Level</p>
           </div>
-          <div class="bg-gray-800 p-4 rounded-lg text-center">
+          <div class="bg-gray-800 p-4 rounded-3xl text-center">
             <div
-              class="bg-blue-500 w-12 h-12 rounded-full mx-auto flex items-center justify-center"
+              class="bg-blue-500 w-12 h-12 rounded-[20px] mx-auto flex items-center justify-center"
             >
               <i class="fas fa-user"> </i>
             </div>
             <h3 class="text-xl font-bold mt-2">3 SMA</h3>
             <p class="text-sm">Payung</p>
           </div>
-          <div class="bg-gray-800 p-4 rounded-lg text-center">
+          <div class="bg-gray-800 p-4 rounded-3xl text-center">
             <div
-              class="bg-blue-500 w-12 h-12 rounded-full mx-auto flex items-center justify-center"
+              class="bg-blue-500 w-12 h-12 rounded-[20px] mx-auto flex items-center justify-center"
             >
               <i class="fas fa-user"> </i>
             </div>
-            <h3 class="text-xl font-bold mt-2">Susus 5 Juz</h3>
+            <h3 class="text-xl font-bold mt-2">Sisus 30 Juz</h3>
             <p class="text-sm">PLP Tahfidz</p>
           </div>
           <div
-            class="bg-gray-800 p-4 rounded-lg text-center col-span-2 md:col-span-4"
+            class="bg-gray-800 p-4 rounded-3xl text-center col-span-2 md:col-span-4"
           >
             <div
-              class="bg-blue-500 w-12 h-12 rounded-full mx-auto flex items-center justify-center"
+              class="bg-blue-500 w-12 h-12 rounded-[20px] mx-auto flex items-center justify-center"
             >
               <i class="fas fa-user"> </i>
             </div>
@@ -83,7 +84,7 @@
             <!-- Achievements Section -->
             <div>
               <h2 class="text-xl font-bold">Pencapaian</h2>
-              <div class="bg-gray-800 p-4 rounded-lg mt-2">
+              <div class="bg-gray-800 p-4 rounded-3xl mt-2">
                 <div class="flex items-center mb-2">
                   <i class="fas fa-trophy text-yellow-400"></i>
                   <div class="ml-2">
@@ -135,19 +136,19 @@
           </div>
           <div class="bg-gray-800 p-8 rounded-3xl flex space-x-8 items-end">
             <div class="flex flex-col items-center">
-              <div class="bg-green-300 w-20 h-32 rounded-full"></div>
+              <div class="bg-green-300 w-17 h-36 rounded-full"></div>
               <p class="text-white mt-4">Tahfidz</p>
             </div>
             <div class="flex flex-col items-center">
-              <div class="bg-blue-500 w-20 h-48 rounded-full"></div>
+              <div class="bg-blue-500 w-17 h-52 rounded-full"></div>
               <p class="text-white mt-4">IT</p>
             </div>
             <div class="flex flex-col items-center">
-              <div class="bg-orange-500 w-20 h-40 rounded-full"></div>
+              <div class="bg-orange-500 w-17 h-44 rounded-full"></div>
               <p class="text-white mt-4">Karakter</p>
             </div>
             <div class="flex flex-col items-center">
-              <div class="bg-yellow-400 w-20 h-36 rounded-full"></div>
+              <div class="bg-yellow-400 w-17 h-40 rounded-full"></div>
               <p class="text-white mt-4">Bahasa</p>
             </div>
           </div>
