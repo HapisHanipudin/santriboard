a360daa4cf027b6c3ffdb68552538e0aeae8dfca
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2025-05-20 14:56:02 +0700

    LESGOOO

--- components/class/student/card.vue
+++ components/class/student/card.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-[#1D1E21] px-3 py-5 rounded-2xl flex items-center flex-col gap-2 border-[0.5px] border-[#3E3E40]">
     <div class="rounded-lg w-32 h-auto aspect-square overflow-hidden">
-      <img :src="`https://app.dreamapps.id/storage/students/photo/${student.nis}.webp`" class="object-cover object-top" alt="" />
+      <img :src="`https://app.dreamapps.id/storage/students/photo/${student.student.nis}.webp`" class="object-cover object-top" alt="" />
     </div>
     <div class="flex flex-col items-center gap-2">
-      <p class="font-semibold text-center truncate">{{ student.name }}</p>
+      <p class="font-semibold text-center truncate">{{ student.student.name }}</p>
     </div>
     <div class="flex gap-3">
       <button class="py-2 px-5 rounded-lg bg-white flex items-center justify-center"><UIcon name="gravity-ui:pencil-to-line" class="text-[#3E62FE]" size="25" /></button>
@@ -14,11 +14,20 @@
 </template>
 
 <script lang="ts" setup>
-const { student } = defineProps<{
+const { student, kelas } = defineProps<{
   student: {
-    nis: string;
+    id: string;
+    studentId: string;
+    semesterId: string;
+    student: {
+      name: string;
+      nis: string;
+    };
+  };
+  kelas: {
     id: string;
     name: string;
+    divisionId: string;
   };
 }>();
 </script>
