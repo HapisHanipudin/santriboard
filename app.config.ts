aa8ffa23be8ad447ea7c55a3cc1847fa6f52ce6b
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2025-05-21 09:25:22 +0700

    lesgooo

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,7 @@ export default defineAppConfig({
       variants: {
         fullscreen: {
           false: {
-            content: "sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl ",
+            content: "sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl rounded-2xl",
           },
         },
       },
--- components/form/assesment/tahfizh.vue
+++ components/form/assesment/tahfizh.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="p-6 h-(--container-4xl) flex flex-col items-center justify-center gap-4">
+  <div class="p-6 h-(--container-2xl) rounded-2xl flex flex-col items-center justify-center gap-4">
     <div class="flex flex-col items-center">
       <h3 class="text-2xl font-semibold">{{ student.student.name }}</h3>
-      <p class="text-gray-500">{{ kelas.name }}</p>
+      <p class="text-gray-500 text-lg">{{ kelas.name }}</p>
     </div>
     <UForm class="w-full max-w-2xl flex flex-col gap-2">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
@@ -14,28 +14,28 @@
               }"
               step="0.5"
               v-model="state.pageCount"
-              class="w-full"
+              class="w-full rounded-xl"
               size="xl"
               placeholder="Jumlah Setoran"
             />
           </UFormField>
           <UFormField name="page" label="Halaman">
-            <UInput v-model="state.page" class="w-full" size="xl" placeholder="Halaman yang disetorkan" />
+            <UInput v-model="state.page" class="w-full rounded-xl" size="xl" placeholder="Halaman yang disetorkan" />
           </UFormField>
           <UFormField name="frequency" label="Frekuensi">
-            <USelect v-model="state.frequency" class="w-full" size="xl" trailing-icon="gravity-ui:pencil-to-square" :items="frequencies" />
+            <USelect v-model="state.frequency" class="w-full rounded-xl" size="xl" trailing-icon="gravity-ui:pencil-to-square" :items="frequencies" />
           </UFormField>
         </div>
         <div class="flex flex-col gap-3">
           <UFormField name="mistakeCount" label="Jumlah Dikasih Tau">
-            <UInputNumber v-model="state.mistakeCount" class="w-full" size="xl" placeholder="Jumlah Dikasih Tau" />
+            <UInputNumber v-model="state.mistakeCount" class="w-full rounded-xl" size="xl" placeholder="Jumlah Dikasih Tau" />
           </UFormField>
           <UFormField name="repeatedCount" label="Jumlah Dikasih Tau">
-            <UInputNumber v-model="state.repeatedCount" class="w-full" size="xl" placeholder="Jumlah Dikasih Tau" />
+            <UInputNumber v-model="state.repeatedCount" class="w-full rounded-xl" size="xl" placeholder="Jumlah Dikasih Tau" />
           </UFormField>
           <UFormField label="Tanggal Setoran">
             <UPopover>
-              <UButton class="w-full" size="xl" color="neutral" variant="subtle" icon="i-lucide-calendar">
+              <UButton class="w-full rounded-xl" size="xl" color="neutral" variant="subtle" icon="i-lucide-calendar">
                 {{ modelValue ? df.format(modelValue.toDate(getLocalTimeZone())) : "Select a date" }}
               </UButton>
 
@@ -45,9 +45,13 @@
             </UPopover>
           </UFormField>
         </div>
-        <UFormField name="note" label="Catatan" class=" ">
-          <UTextarea v-model="state.note" class="w-full" size="xl" placeholder="Catatan" />
+        <UFormField name="note" label="Catatan" class="md:col-span-2">
+          <UTextarea v-model="state.note" class="w-full rounded-xl" size="xl" placeholder="Catatan" />
         </UFormField>
+      </div>
+      <div class="flex justify-end gap-2">
+        <UButton color="neutral" variant="subtle" size="xl" class="w-full max-w-[200px]"> Batalkan </UButton>
+        <UButton color="primary" size="xl" class="w-full max-w-[200px]"> Simpan </UButton>
       </div>
     </UForm>
   </div>
